71e5da94cff3b72db46b46e0152592d07e50c0e4
Gabriele M <moto.falcon.git@gmail.com>
2016-02-16 14:18:31 +0100

    Add fstab for GPE variant
    The GPE variant has an extra partition to store encryption metadata,
    so the fstab must be adjusted for proper decryption. However, it
    might be possible for a GPE device to have the encryption metadata
    in the footer. Add an additional fstab for GPE, but use it only if
    the device doesn't have encryption metadata in the footer of userdata.

--- omni_falcon.mk
+++ omni_falcon.mk
@@ -26,6 +26,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 # Inherit from our custom product configuration
 $(call inherit-product, vendor/omni/config/common.mk)
 
+PRODUCT_PACKAGES += \
+    detect-gpe
+
 PRODUCT_NAME := omni_falcon
 PRODUCT_DEVICE := falcon
 PRODUCT_BRAND := Motorola
